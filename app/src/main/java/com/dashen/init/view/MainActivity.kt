4622ee8557dcc287b4eedb4b774bb727e16a8b61
oAvAo <1501593671@qq.com>
2018-04-09 14:36:54 +0800

    Kotlin Android Extensions 替代findViewById()

--- app/src/main/java/com/dashen/init/view/MainActivity.kt
+++ app/src/main/java/com/dashen/init/view/MainActivity.kt
@@ -6,6 +6,7 @@ import com.dashen.init.App.Companion.context
 import com.dashen.init.R
 import com.dashen.init.presenter.MainHelper
 import com.dashen.init.presenter.viewinter.MainView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), MainView {
     private var mMainHelper: MainHelper? = null//MainActivity业务处理类
@@ -15,5 +16,7 @@ class MainActivity : AppCompatActivity(), MainView {
         setContentView(R.layout.activity_main)
         mMainHelper = MainHelper(context!!, this)
         mMainHelper?.getBannerData()
+
+        tv.text = "hello"
     }
 }
