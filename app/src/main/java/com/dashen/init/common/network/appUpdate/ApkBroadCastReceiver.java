312f4206760166bc79cd48f0a19e91fe779ddc47
beibei <1501593671@qq.com>
2018-10-17 18:09:27 +0800

    版本更新xxx

--- app/src/main/java/com/dashen/init/common/network/appUpdate/ApkBroadCastReceiver.java
+++ app/src/main/java/com/dashen/init/common/network/appUpdate/ApkBroadCastReceiver.java
@@ -3,7 +3,10 @@ package com.dashen.init.common.network.appUpdate;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.Toast;
+
+import com.dashen.utils.LogUtils;
 
 /**
  *
@@ -12,20 +15,25 @@ import android.widget.Toast;
 public class ApkBroadCastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
 
-        if (Intent.ACTION_PACKAGE_ADDED.equals(intent.getAction())) {
+        Uri uri = intent.getData();
+        if (Intent.ACTION_PACKAGE_ADDED.equals(action)) {
 //            comm.rmoveFile("http://imtt.dd.qq.com/16891/7C7BB50B68B684A36339AF1F615E2848.apk");
-            Toast.makeText(context, "监听到系统广播添加", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "监听到系统广播添加"+uri.toString(), Toast.LENGTH_LONG).show();
+            LogUtils.e("-------------监听到系统广播添加"+uri.toString());
         }
 
-        if (Intent.ACTION_PACKAGE_REMOVED.equals(intent.getAction())) {
+        if (Intent.ACTION_PACKAGE_REMOVED.equals(action)) {
 //            comm.rmoveFile("http://imtt.dd.qq.com/16891/7C7BB50B68B684A36339AF1F615E2848.apk");
-            Toast.makeText(context, "监听到系统广播移除", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "监听到系统广播移除"+uri.toString(), Toast.LENGTH_LONG).show();
+            LogUtils.e("-------------监听到系统广播添加"+uri.toString());
         }
 
-        if (Intent.ACTION_PACKAGE_REPLACED.equals(intent.getAction())) {
+        if (Intent.ACTION_PACKAGE_REPLACED.equals(action)) {
 //            comm.rmoveFile("http://imtt.dd.qq.com/16891/7C7BB50B68B684A36339AF1F615E2848.apk");
-            Toast.makeText(context, "监听到系统广播替换", Toast.LENGTH_LONG).show();
+            Toast.makeText(context, "监听到系统广播替换"+uri.toString(), Toast.LENGTH_LONG).show();
+            LogUtils.e("-------------监听到系统广播添加"+uri.toString());
         }
     }
 }
